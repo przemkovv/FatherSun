82feacd2c821d3d94f296b88a922650868dd8f29
Przemyslaw Walkowiak <przemkovv@gmail.com>
2016-12-15 15:14:48 +0100

    Implementacja ciągu fibonacciego

--- FatherSun/main.cpp
+++ FatherSun/main.cpp
@@ -3,6 +3,22 @@
 
 #include <spdlog/spdlog.h>
 
+
+int64_t fibonnacci_number(int n)
+{
+	int64_t f0 = 0;
+	int64_t f1 = 1;
+	int64_t f2 = f1;
+
+	for (int i = 0; i < n; i++)
+	{
+		f2 = f1 + f0;
+		f0 = f1;
+		f1 = f2;;
+		}
+	return f2;
+}
+
 int main()
 {
 
@@ -14,5 +30,17 @@ int main()
 	logger->info("Dzisiaj techday");
 	logger->debug("Mówimy o C++");
 	logger->error("Mieliœmy opóŸnienie");
+	logger->info("{}", fibonnacci_number(0));
+
+	logger->info("{}", fibonnacci_number(1));
+	logger->info("{}", fibonnacci_number(2));
+
+	logger->info("{}", fibonnacci_number(3));
+	logger->info("{}", fibonnacci_number(4));
+	logger->info("{}", fibonnacci_number(5));
+	logger->info("{}", fibonnacci_number(6));
+
+
+
 	return 0;
 }
